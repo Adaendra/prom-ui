ec8080161b41dcd9c71a6572ca89e7aaf117d62a
Maxime G <36586573+Adaendra@users.noreply.github.com>
2022-09-29 08:09:18 -0400

    #7 - Add health check (#8)

--- react-app/src/setupProxy.js
+++ react-app/src/setupProxy.js
@@ -1,11 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: process.env.PROMETHEUS_SRC_API,
-      changeOrigin: true,
-    })
-  );
+module.exports = function (app) {
+    app.get('/health-check', (req, res) => {
+        res.send("OK");
+    });
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: process.env.PROMETHEUS_SRC_API,
+            changeOrigin: true,
+        })
+    );
 };
